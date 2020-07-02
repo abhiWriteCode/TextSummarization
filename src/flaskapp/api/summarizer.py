301e3994dd6b95aace7778a3a185e95e3e9e49fc
abhiWriteCode <abhigoswami2106@gmail.com>
2020-07-02 11:19:55 +0530

    reqparse added

--- src/flaskapp/api/summarizer.py
+++ src/flaskapp/api/summarizer.py
@@ -1,14 +1,20 @@
 from flask import request
-from flask_restful import Resource, abort
+from flask_restful import Resource, reqparse, abort
 
 from ..model import get_summary
+
+
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument('text',  type=str, required=True)
+parser.add_argument('max_length',  type=int, required=True)
 
 
 class Summarizer(Resource):
 
     def post(self):
-        long_text = request.form.get('text', None)
-        max_length = request.form.get('max_length', None)
+        args = parser.parse_args(strict=True)
+        long_text = args['text']
+        max_length = args['max_length']
 
         if long_text is None or max_length is None:
             abort(404, message='Text or max_length is not available')
